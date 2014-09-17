0209dbfcfee743175d093dbae049cfc2ea215e4b
Sebastian Geiger <sebastian@bignerdranch.com>
2014-09-17 14:27:59 -0400

    Handling Cargo and keeping track of Canada

--- lib/application.rb
+++ lib/application.rb
@@ -1,3 +1,4 @@
+# 1. Events
 class DomainEvent
   attr_reader :recorded, :occurred
   def initialize(occurred: Time.now)
@@ -27,6 +28,27 @@ class DepartureEvent < ShipMovementEvent
   end
 end
 
+class CargoEvent < DomainEvent
+  attr_reader :date, :cargo, :ship
+  def initialize(date: Date.today, cargo:, ship: )
+    super(occurred: date)
+    @cargo = cargo
+    @ship = ship
+  end
+end
+
+class LoadEvent < CargoEvent
+  def process
+    @ship.handle_load(self)
+  end
+end
+
+class UnloadEvent < CargoEvent
+  def process
+    @ship.handle_unload(self)
+  end
+end
+
 class EventProcessor
   def initialize
     @event_list = []
@@ -38,19 +60,48 @@ class EventProcessor
   end
 end
 
+# 2. Domain Models
 class Ship < Struct.new(:name)
   attr_reader :location
   def initialize(name)
     super(name)
     @location = :unknown
+    @cargo = Set.new
   end
   def handle_arrival(event)
     @location = event.port
+    @cargo.each {|c| c.handle_arrival(event)}
   end
   def handle_departure(event)
     @location = :at_sea
+    @cargo.each {|c| c.handle_departure(event)}
+  end
+  def handle_load(event)
+    @cargo.add(event.cargo)
+  end
+  def handle_unload(event)
+    @cargo.delete(event.cargo)
   end
 end
 
-class Port < Struct.new(:city, :country)
+class Port
+  attr_reader :city, :country
+  def initialize(city:, country: )
+    @city = city
+    @country = country
+  end
+end
+
+class Cargo < Struct.new(:name)
+  def initialize(name)
+    super(name)
+    @has_been_in_canada = false
+  end
+  def has_been_in_canada?
+    @has_been_in_canada
+  end
+  def handle_arrival(event)
+    @has_been_in_canada = true if event.port.country == :Canada
+  end
+  def handle_departure(event); end
 end
--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -8,7 +8,7 @@ describe 'Updating the application model' do
   let(:la) { Port.new(city: "Los Angeles", country: :USA) }
   let(:yyv) { Port.new(city: "Vancouver", country: :Canada) }
   let(:ship) { Ship.new("King Roy") }
-  let(:cargo) { Cargo.new("Refactoring") }
+  let(:refactoring_books) { Cargo.new("Refactoring") }
   let(:event_processor) { EventProcessor.new }
 
   it 'sets the ships location on arrival' do
@@ -25,5 +25,30 @@ describe 'Updating the application model' do
     ]
     event_processor.process(events)
     expect(ship.location).to eql :at_sea
+  end
+
+  describe 'keeping track of Canada' do
+    it 'records that it has not been to canda' do
+      events = [
+        LoadEvent.new(     date: Time.new(2005,11,01), ship: ship, cargo: refactoring_books),
+        DepartureEvent.new(date: Time.new(2005,11,02), ship: ship, port: la),
+        ArrivalEvent.new(  date: Time.new(2005,11,24), ship: ship, port: sf),
+        UnloadEvent.new(   date: Time.new(2005,11,25), ship: ship, cargo: refactoring_books)
+      ]
+      event_processor.process(events)
+      expect(refactoring_books).to_not have_been_in_canada
+    end
+    it 'records that it passed through Canada' do
+      events = [
+        LoadEvent.new(     date: Time.new(2005,11,01), ship: ship, cargo: refactoring_books),
+        DepartureEvent.new(date: Time.new(2005,11,02), ship: ship, port: la),
+        ArrivalEvent.new(  date: Time.new(2005,11,13), ship: ship, port: yyv),
+        DepartureEvent.new(date: Time.new(2005,11,14), ship: ship, port: yyv),
+        ArrivalEvent.new(  date: Time.new(2005,11,24), ship: ship, port: sf),
+        UnloadEvent.new(   date: Time.new(2005,11,25), ship: ship, cargo: refactoring_books)
+      ]
+      event_processor.process(events)
+      expect(refactoring_books).to have_been_in_canada
+    end
   end
 end
